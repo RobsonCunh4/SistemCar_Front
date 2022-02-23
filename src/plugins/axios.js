defd36a8b5259ecef170bb3a10b07b8179764e03
StrongRC <95380035+StrongRC@users.noreply.github.com>
2022-02-23 19:14:15 -0300

    Atualização base url produção

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import Cookie from "@/service/cookie";
 
-axios.defaults.baseURL = 'http://localhost:8000/api';
+// axios.defaults.baseURL = 'http://localhost:8000/api'; base Url local
+axios.defaults.baseURL = 'https://sistemcarback.herokuapp.com';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
