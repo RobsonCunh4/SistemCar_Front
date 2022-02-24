2a8b54b7e364a9a119e82649cbeada5b58595838
Robson Cunha <95380035+StrongRC@users.noreply.github.com>
2022-02-24 05:52:39 -0300

    Update axios.js

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios';
 import Cookie from "@/service/cookie";
 
 // axios.defaults.baseURL = 'http://localhost:8000/api'; base Url local
-axios.defaults.baseURL = 'https://sistemcarback.herokuapp.com/api';
+//axios.defaults.baseURL = 'https://sistemcarback.herokuapp.com/api';
+axios.defaults.baseURL = 'https://sistemcarfront.herokuapp.com/api';
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 
