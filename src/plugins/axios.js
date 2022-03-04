338595dbbccaa28085c13c38693ee4b1fa4ccb77
StrongRC <rcunha.sm@gmail.com>
2022-03-04 00:27:40 -0300

    ajuste

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,8 +3,8 @@ import Cookie from "@/service/cookie";
 
 //axios.defaults.baseURL = 'http://localhost:8000/api';
 axios.defaults.baseURL = 'https://sistemcarback.herokuapp.com/api';
-axios.defaults.headers.common['Content-Type'] = 'application/json';
-axios.defaults.headers.common['Accept'] = 'application/json';
+axios.defaults.headers.common['Content-Type'] = 'text/plain';
+axios.defaults.headers.common['Accept'] = 'text/plain';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 //Vue.prototype.$axios = axios;
